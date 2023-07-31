7c26d8289fcd5f7788d6cf3500cce1ddde9988ba
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-07-31 17:52:46 +0600

    Initial master commit

---
+++ src/main/java/pro/sky/testing_calculator/TestingCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testing_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingCalculatorApplication.class, args);
+    }
+
+}
