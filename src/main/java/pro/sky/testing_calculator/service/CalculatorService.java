71b0ff4aa63faa8b39c5bab3d54bb573eec95ae2
Roman Zelenin (ZelRS) <ZeleninRS1991@gmail.com>
2023-07-31 20:35:30 +0600

    Выполнил домашнее задание по уроку "Тестирование"

---
+++ src/main/java/pro/sky/testing_calculator/service/CalculatorService.java
@@ -0,0 +1,13 @@
+package pro.sky.testing_calculator.service;
+
+public interface CalculatorService {
+    String sayHallo();
+
+    int getSum(int num1, int num2);
+
+    int getMinus(int num1, int num2);
+
+    int getMultiply(int num1, int num2);
+
+    int getDivide(int num1, int num2);
+}
---
+++ src/test/java/pro/sky/testing_calculator/service/impl/CalculatorServiceImplParameterizedTest.java
@@ -0,0 +1,60 @@
+package pro.sky.testing_calculator.service.impl;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.boot.test.context.SpringBootTest;
+import pro.sky.testing_calculator.service.CalculatorService;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static pro.sky.testing_calculator.constants.CalculatorServiceTestConstants.*;
+@SpringBootTest
+public class CalculatorServiceImplParameterizedTest {
+
+    private final CalculatorService out = new CalculatorServiceImpl();
+    private int resultExpected;
+    private int resultActual;
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTest")
+    public void ShouldCurrentSum(int num1, int num2) {
+        resultExpected = num1 + num2;
+        resultActual = out.getSum(num1, num2);
+        assertEquals(resultExpected, resultActual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTest")
+    public void ShouldCurrentMinus(int num1, int num2) {
+        resultExpected = num1 - num2;
+        resultActual = out.getMinus(num1, num2);
+        assertEquals(resultExpected, resultActual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTest")
+    public void ShouldCurrentMultiply(int num1, int num2) {
+        resultExpected = num1 * num2;
+        resultActual = out.getMultiply(num1, num2);
+        assertEquals(resultExpected, resultActual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideParamsForTest")
+    public void ShouldCurrentDivide(int num1, int num2) {
+        resultExpected = num1 / num2;
+        resultActual = out.getDivide(num1, num2);
+        assertEquals(resultExpected, resultActual);
+    }
+
+    public static Stream<Arguments> provideParamsForTest() {
+        return Stream.of(
+                Arguments.of(RANDOM_NUM1, RANDOM_NUM2),
+                Arguments.of(RANDOM_NUM3, RANDOM_NUM4),
+                Arguments.of(RANDOM_NUM1, RANDOM_NUM3),
+                Arguments.of(RANDOM_NUM2, RANDOM_NUM4)
+        );
+    }
+}
